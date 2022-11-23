0b2d86aae3015c2051eb6a9b47122eb0387d3c0f
Devin Lizardi <dlizardi@berkeley.edu>
2022-11-23 13:32:08 -0500

    fix: styled with svg

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -3,25 +3,31 @@
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content">
       <NavBarVue />
-      <label for="my-drawer" class="btn btn-primary drawer-button md:hidden">Open drawer</label>
-    </div> 
+      <label
+        for="my-drawer"
+        class="btn btn-ghost hover:btn-accent drawer-button md:hidden absolute top-6 left-6 text-white px-3"
+      >
+        <svgMenuVue />
+      </label>
+    </div>
     <div class="drawer-side">
       <label for="my-drawer" class="drawer-overlay"></label>
       <ul class="menu p-4 w-80 bg-base-100 text-base-content">
         <!-- Sidebar content here -->
         <li><a>Sidebar Item 1</a></li>
         <li><a>Sidebar Item 2</a></li>
-        
       </ul>
     </div>
   </div>
 </template>
 
 <script>
-import NavBarVue from './NavBar.vue';
+import NavBarVue from "./NavBar.vue"
+import svgMenuVue from "./svgMenu.vue"
 export default {
   components: {
-    NavBarVue
-  }
+    NavBarVue,
+    svgMenuVue,
+  },
 }
 </script>
---
+++ src/components/svgMenu.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg class="h-8 w-8" viewBox="0 0 292 292" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+    <line x1="25.5" y1="65.5" x2="266.5" y2="65.5" stroke-width="31" stroke-linecap="round" />
+    <line x1="25.5" y1="145.5" x2="266.5" y2="145.5" stroke-width="31" stroke-linecap="round" />
+    <line x1="25.5" y1="225.5" x2="266.5" y2="225.5" stroke-width="31" stroke-linecap="round" />
+  </svg>
+</template>
