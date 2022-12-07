01f828c165eb43b3a9ed6ffd1bc49fea65a3696f
Devin Lizardi <dlizardi@berkeley.edu>
2022-12-07 14:18:49 -0500

    finished flexbox in modal

--- src/components/NewsModal.vue
+++ src/components/NewsModal.vue
@@ -3,10 +3,10 @@
   <label :for="modalId" class="modal cursor-pointer">
     <label class="modal-box relative w-11/12 max-w-4xl">
       <div class="grid grid-cols-1 place-items-center">
-        <div class="row-start-1 flex flex-wrap w-fit justify-evenly items-center">
-          <span class="badge badge-primary uppercase m-0">{{ newsType }}</span>
-          <h3 class="text-lg font-bold">{{ title }}</h3>
-          <p>{{ date }}</p>
+        <div class="row-start-1 flex flex-wrap w-full items-center mb-4">
+          <span class="badge badge-primary uppercase float-left">{{ newsType }}</span>
+          <h3 class="text-2xl font-bold grow text-center">{{ title }}</h3>
+          <p class="float-right grow-0">{{ date }}</p>
         </div>
         <figure class="my-1 row-start-2 w-full"><img class="w-full h-auto" :src="imgUrl" :alt="altText" /></figure>
       </div>
