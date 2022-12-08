690511c2f84137b7439f894e064f0dfc175e4674
Devin Lizardi <dlizardi@berkeley.edu>
2022-12-08 15:33:22 -0500

    set min width for cards

--- src/views/Media.vue
+++ src/views/Media.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>helo media</h1>
+  <div>
+    <div class="hero h-24 md:h-32 bg-cover relative" :style="`background-image: url(${bgSliderUrl});`">
+      <div class="flex flex-col justify-center">
+        <h1 class="text-3xl md:text-5xl text-center">Media</h1>
+        <figure class="w-32 md:w-44 mt-1"><img :src="bgUnderLineUrl" :alt="altText" /></figure>
+      </div>
+    </div>
+    <div
+      class="mt-5 w-screen grid place-items-center justify-center overflow-x-hidden gap-4 grid-cols-[minmax(0,_100px)_minmax(auto,_80rem)_minmax(0,_100px)]"
+    >
+      <div class="row-start-1 col-start-2 p-8 flex place-content-center">
+        <div v-for="_ in [1, 2, 3]">
+          <EventCardVue
+          :imgUrl="newsUrls[0]"
+          class="max-w-[10px]"
+          modalId="aieIsland"
+          altText="test"
+          newsType="event"
+          title="Prepare for Aie Island!"
+          date="2022.12.1"
+        />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import EventCardVue from "../components/EventCard.vue"
+import bgSliderUrl from "./../../static/assets/lt-slider-misc.jpg?url"
+import bgUnderLineUrl from "./../../static/assets/lt_arrow.png"
+const newsUrls = ["https://placeimg.com/400/225/arch"]
+
+export default {
+  data() {
+    return { bgSliderUrl, bgUnderLineUrl, newsUrls }
+  },
+  components: { EventCardVue }
+}
+</script>
