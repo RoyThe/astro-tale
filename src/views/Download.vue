7bd41b742e5388d71953c0d09db29d2455e13b9a
Devin Lizardi <dlizardi@berkeley.edu>
2022-12-12 15:22:57 -0500

    refactor

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="scroll-smooth">
     <div class="hero min-h-screen" :style="`background-image: url(${bgUrl});`">
-      <div class="hero-content text-center text-neutral-content bg-neutral rounded-lg w-10/12 max-w-6xl relative -top-16">
+      <div
+        class="hero-content text-center text-neutral-content bg-neutral rounded-lg 
+        w-10/12 max-w-6xl relative -top-16"
+      >
         <div class="max-w-[90%] py-4">
           <p class="md:text-2xl">
             Embark on an epic quest with other Heroes from around the globe to battle against the forces of the Demon
             World and search for the renowned champion Iris Riviere.
           </p>
           <br />
-          <h2 class="text-2xl md:text-4xl font-extrabold font-mono max-w-full">Download La Tale now and TELL YOUR TALE today!</h2>
+          <h2 class="text-2xl md:text-4xl font-extrabold font-mono max-w-full">
+            Download La Tale now and TELL YOUR TALE today!
+          </h2>
           <br />
-          <a href="#downloads-table" class="btn btn-primary min-w-[200px] w-6/12 h-auto text-lg font-extrabold md:text-2xl">View Downloads</a>
+          <a
+            href="#downloads-table"
+            class="btn btn-primary min-w-[200px] w-6/12 h-auto text-lg font-extrabold md:text-2xl"
+            >View Downloads</a
+          >
         </div>
       </div>
     </div>
@@ -19,9 +28,7 @@
     >
       <div class="row-start-1 col-start-2 flex flex-wrap place-content-center" id="downloads-table">
         <h1>Downloads</h1>
-        <div class="w-full h-40 bg-slate-400">
-
-        </div>
+        <div class="w-full h-40 bg-slate-400"></div>
       </div>
     </div>
   </div>
