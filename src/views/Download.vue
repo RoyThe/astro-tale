eba217105beb5775335eff80acdf92845323c5fe
Devin Lizardi <dlizardi@berkeley.edu>
2022-12-12 15:10:33 -0500

    finishing center hero elem

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
-    <div class="hero min-h-screen" :style="`background-image: url(${bgUrl});`">
-      <img class="w-32 h-auto relative -top-40" src="./../../static/assets/lt_logo.png" />
-      <div class="hero-content text-center text-neutral-content bg-neutral rounded max-w-6xl">
-        <div class="max-w-md">
-          <p>
+    <div class="hero min-h-screen relative -top-1 -z-10" :style="`background-image: url(${bgUrl});`">
+      <div class="hero-content text-center text-neutral-content bg-neutral rounded-lg w-10/12 max-w-6xl z-0 relative -top-16">
+        <div class="max-w-[90%] py-4">
+          <p class="md:text-2xl">
             Embark on an epic quest with other Heroes from around the globe to battle against the forces of the Demon
             World and search for the renowned champion Iris Riviere.
           </p>
-          <h2 class="text-2xl font-extrabold font-mono max-w-full">Download La Tale now and TELL YOUR TALE today!</h2>
-          <a class="btn btn-primary">View Downloads</a>
+          <br />
+          <h2 class="text-2xl md:text-4xl font-extrabold font-mono max-w-full">Download La Tale now and TELL YOUR TALE today!</h2>
+          <br />
+          <a href="#downloads-table" class="btn btn-primary min-w-[200px] w-6/12 h-auto text-lg font-extrabold md:text-2xl">View Downloads</a>
         </div>
       </div>
     </div>
     <div
       class="mt-5 w-screen grid place-items-center justify-center overflow-x-hidden gap-4 grid-cols-[minmax(0,_100px)_minmax(auto,_80rem)_minmax(0,_100px)]"
     >
-      <div class="row-start-1 col-start-2 flex flex-wrap place-content-center">
+      <div class="row-start-1 col-start-2 flex flex-wrap place-content-center" id="downloads-table">
         <h1>Downloads</h1>
+        <div class="w-full h-40 bg-slate-400">
+
+        </div>
       </div>
     </div>
   </div>
