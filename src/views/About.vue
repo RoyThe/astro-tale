303e6b8a00b1e91db8fcd780e77d10f7fd624f19
Devin Lizardi <dlizardi@berkeley.edu>
2023-01-24 22:57:35 -0800

    about alert

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>abuuut</h1>
+  <div
+    class="mt-5 w-full grid place-items-center justify-center overflow-x-hidden gap-4 grid-cols-[minmax(0,_100px)_minmax(auto,_80rem)_minmax(0,_100px)]"
+  >
+    <div class="alert alert-error shadow-lg row-start-1 col-start-2">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="stroke-current flex-shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>Error! Currently under development</span>
+      </div>
+    </div>
+  </div>
 </template>
