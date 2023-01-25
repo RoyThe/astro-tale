e8d0edd048bba7cfe747470ac80f9bd315e3e9a8
Devin Lizardi <dlizardi@berkeley.edu>
2023-01-24 16:06:45 -0800

    fix divider, show more button

--- src/views/News.vue
+++ src/views/News.vue
@@ -7,14 +7,14 @@
       </div>
     </div>
     <div
-      class="mt-5 w-full grid place-items-center justify-center overflow-x-hidden gap-4 grid-cols-[minmax(0,_100px)_minmax(auto,_80rem)_minmax(0,_100px)]"
+      class="mt-5 pb-16 w-full grid place-items-center justify-center overflow-x-hidden gap-4 grid-cols-[minmax(0,_100px)_minmax(auto,_80rem)_minmax(0,_100px)]"
     >
-      <div class="row-start-1 col-start-2 text-neutral-content justify-self-start text-3xl">
+      <div class="row-start-1 col-start-2 text-neutral-content justify-self-start w-full text-3xl">
         <h1>All</h1>
-        <div class="divider w-full"/>
+        <div class="divider"/>
       </div>
-      <div class="mb-16 row-start-2 col-start-2 flex flex-wrap place-content-center">
-        <div v-for="_ in [1, 2, 3, 4, 5, 6]" class="max-w-[250px]">
+      <div class="row-start-2 col-start-2 flex flex-wrap place-content-center">
+        <div v-for="_ in parseInt(cardLimit)" class="max-w-[250px]">
           <EventCardVue
             :imgUrl="newsUrls[0]"
             modalId="aieIsland"
@@ -25,6 +25,7 @@
           />
         </div>
       </div>
+      <button v-if="cardLimit < 13" @click="cardLimit+=3" class="mt-4 btn btn-outline row-start-3 col-start-2"> Show More </button>
     </div>
   </div>
 </template>
@@ -33,11 +34,13 @@
 import EventCardVue from "../components/EventCard.vue"
 import bgSliderUrl from "./../../static/assets/lt-slider-misc.jpg?url"
 import bgUnderLineUrl from "./../../static/assets/lt_arrow.png"
+
 const newsUrls = ["https://placeimg.com/400/225/arch"]
+const cardLimit = 6
 
 export default {
   data() {
-    return { bgSliderUrl, bgUnderLineUrl, newsUrls }
+    return { bgSliderUrl, bgUnderLineUrl, newsUrls, cardLimit }
   },
   components: { EventCardVue },
 }
