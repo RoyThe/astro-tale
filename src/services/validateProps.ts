6922dcdbac3cf5742a13f86718562270f0d10539
Devin Lizardi <dlizardi@berkeley.edu>
2023-02-01 02:06:19 -0500

    reference comment

--- src/services/validateProps.ts
+++ src/services/validateProps.ts
@@ -1,5 +1,14 @@
-import exp from "constants"
 import { News } from "./types"
+
+// const isParsedArgs = (args: unknown): args is ParsedArgs => 
+//   typeof args == "object"
+//   && 'daily_exercises' in (args as ParsedArgs)
+//   && 'target' in (args as ParsedArgs);
+
+// const isValidData = (args: ParsedArgs): args is ParsedArgs =>
+//   !isNaN(Number(args.target))
+//   && Array.isArray(args.daily_exercises)
+//   && args.daily_exercises.filter(num => isNaN(Number(num))).length === 0;
 
 const checkForBodyProp = (arr: News[]): void | Error => {
   arr.forEach((item) => {
