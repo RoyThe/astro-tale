ed89c7391c60a4b7b7d1ad57751be2ea064af120
Devin Lizardi <dlizardi@berkeley.edu>
2023-02-03 21:06:35 -0500

    added about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div
     class="mt-5 w-full grid place-items-center justify-center overflow-x-hidden gap-4 grid-cols-[minmax(0,_100px)_minmax(auto,_80rem)_minmax(0,_100px)]"
   >
-    <div class="alert alert-error shadow-lg row-start-1 col-start-2">
+    <!-- <div class="alert alert-error shadow-lg row-start-1 col-start-2">
       <div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -19,6 +19,65 @@
         </svg>
         <span>Error! Currently under development</span>
       </div>
-    </div>
+    </div> -->
+
+    <article class="row-start-1 col-start-2 prose">
+      <h1>Discover the Thrilling World of Latale: A Comprehensive MMORPG</h1>
+
+      <p>
+        Latale is a massively multiplayer online role-playing game (MMORPG) that offers players a unique and exciting
+        gaming experience. With its blend of classic role-playing game mechanics and real-time strategy elements, Latale
+        offers endless opportunities for players to explore, battle, and interact with each other.
+      </p>
+
+      <h2>Gameplay Features</h2>
+
+      <h3>Battling Monsters</h3>
+      <p>
+        Latale's world is populated with a wide variety of monsters, from small and weak creatures to powerful bosses.
+        Battling these monsters is a key part of Latale's gameplay, as it allows players to gain experience, loot, and
+        grow their characters. Whether you prefer to solo or team up with other players, Latale's monster battles offer
+        a thrilling and challenging experience.
+      </p>
+
+      <h3>Exploration</h3>
+      <p>
+        Latale's world is vast and filled with secrets to uncover. From hidden dungeons to secret locations, Latale's
+        exploration system offers players the chance to discover new and exciting things. Whether you're searching for
+        treasure or just exploring the world, Latale's exploration system is a key part of the game's appeal.
+      </p>
+
+      <h2>Interacting with Other Players</h2>
+
+      <h3>Trading</h3>
+      <p>
+        Latale's trading system allows players to trade items, equipment, and more with each other. Whether you're
+        looking for rare items or just trying to make a profit, Latale's trading system offers a variety of
+        opportunities for players to engage with each other.
+      </p>
+
+      <h3>Forming Alliances</h3>
+      <p>
+        Latale's alliance system allows players to team up with each other and work together to achieve common goals.
+        Whether you're forming a guild or just forming a temporary alliance, Latale's alliance system offers players the
+        chance to form lasting relationships and work together to achieve their goals.
+      </p>
+
+      <h3>Chatting</h3>
+      <p>
+        Latale's chat system allows players to communicate with each other in real-time. Whether you're seeking advice,
+        making friends, or just chatting with other players, Latale's chat system is a key part of the game's social
+        experience.
+      </p>
+
+      <h2>Conclusion</h2>
+      <p>
+        Latale is a rich and complex MMORPG that offers endless opportunities for players to explore, battle, and
+        interact with each other. Whether you're a seasoned MMORPG veteran or a newcomer to the genre, Latale has
+        something for everyone. With its unique blend of classic role-playing game mechanics and real-time strategy
+        elements, Latale is a game that will captivate and entertain you for hours on end. So grab your sword, grab your
+        spellbook, and join the adventure in Latale today!
+      </p>
+    </article>
   </div>
 </template>
